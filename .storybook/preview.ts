70edcfb31488cc87e917fed03829fe4f64062eb9
Mogeko <zhengjunyi@live.com>
2023-06-03 14:31:02 +0200

    feat: add button from shadcn/ui

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,6 +13,7 @@ const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
+      hideNoControlsWarning: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
---
+++ components/ui/__stories__/button.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { LuMail, LuLoader2 } from "react-icons/lu";
+import { Button } from "@/components/ui/button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as Meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: "Button",
+    variant: "default",
+  },
+};
+
+export const Variants: Story = {
+  render: () => (
+    <div className="flex gap-4">
+      <Button variant="default">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+    </div>
+  ),
+};
+
+export const Sizes: Story = {
+  render: () => (
+    <div className="flex gap-4">
+      <Button size="sm">Small</Button>
+      <Button size="default">Medium</Button>
+      <Button size="lg">Large</Button>
+    </div>
+  ),
+};
+
+export const WithIcon: Story = {
+  render: () => (
+    <Button>
+      <LuMail className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
+  ),
+};
+
+export const Loading: Story = {
+  render: () => (
+    <Button disabled>
+      <LuLoader2 className="animate-spin mr-2 h-4 w-4" />
+      Please wait...
+    </Button>
+  ),
+};

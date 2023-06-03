527d131e288736ee4a1db64955271eb870a303d1
Mogeko <zhengjunyi@live.com>
2023-06-03 02:34:38 +0200

    build: setup storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview, Parameters } from "@storybook/react";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export const parameters: Parameters = {
+  nextjs: {
+    appDirectory: true,
+  },
+};
+
+export default preview;

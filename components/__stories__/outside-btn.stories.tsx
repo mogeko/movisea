0dfa198a2c4d22ad369ac28cffc8b8f345688a48
Mogeko <zhengjunyi@live.com>
2023-06-07 00:34:26 +0200

    fix: further formatting style

--- components/__stories__/outside-btn.stories.tsx
+++ components/__stories__/outside-btn.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import { OutsideButton } from "@/components/outside-btn";
 
 export default {
--- components/__stories__/search.stories.tsx
+++ components/__stories__/search.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useForm } from "react-hook-form";
-import { Search } from "@/components/search";
+
 import { Form } from "@/components/ui/form";
+import { Search } from "@/components/search";
 
 export default {
   title: "Components/Search",

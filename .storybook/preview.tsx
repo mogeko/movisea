708870f51a5c3679eff18c8aca74cf0308bc2b7e
Mogeko <zhengjunyi@live.com>
2023-06-09 21:47:39 +0200

    build: import custom fonts from web for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -21,7 +21,7 @@ export const parameters: Parameters = {
 
 export const decorators: Array<Decorator> = [
   (Story) => (
-    <div className="bg-background">
+    <div className="bg-background p-4">
       <Story />
       <Toaster />
     </div>

6a4b331889c760aa38a15a713a6c3c37ced6a071
Mogeko <zhengjunyi@live.com>
2023-06-11 15:56:19 +0200

    feat: add a sense of design to the logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ const Home: React.FC = () => {
   return (
     <main className="container absolute top-[20%]">
       <div className="flex flex-col items-center justify-center min-h-full py-2">
-        <div className="lg:text-8xl md:text-6xl text-4xl font-bold mb-10">
+        <div className="lg:text-8xl md:text-6xl text-4xl font-bold lg:mb-8 md:mb-4 mb-2">
           {siteConfig.name}
+          <span className="text-destructive">.</span>
         </div>
         <Search className="max-w-[590px] w-full" />
       </div>

67a207b8232d25f8e66fa4947dd951803b0fb151
Mogeko <zhengjunyi@live.com>
2023-06-11 16:44:36 +0200

    feat: add a toggle to switch light/dark mode

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,15 @@
 import { MainNav } from "@/components/main-nav";
 
+import { ModeToggle } from "./mode-toggle";
+
 export const SiteHeader: React.FC = () => {
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center">
         <MainNav />
+        <div className="flex flex-1 items-center justify-between space-x-2 sm:space-x-4 md:justify-end">
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );

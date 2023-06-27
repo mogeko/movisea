2ea50f3b82de4a030874a2823dc98cde1e23f16e
Mogeko <zhengjunyi@live.com>
2023-06-27 18:45:55 +0200

    refactor: rewrite the request with polymorphism

--- packages/tmdb-request/src/mod.ts
+++ packages/tmdb-request/src/mod.ts
@@ -3,10 +3,10 @@ import { parse, type Context } from "@/parse";
 
 export const parser = parse(DEFAULTS);
 
-export async function request<T extends unknown>(
-  route: string,
-  opts: Context = {}
-): Promise<T> {
+export async function request<T>(route: string, opts?: Context): Promise<T>;
+export async function request(route: string, opts?: Context): Promise<any>;
+export async function request(route: string, opts = {}) {
   const { url, ...options } = parser(route, opts);
+
   return (await fetch(url, options)).json();
 }

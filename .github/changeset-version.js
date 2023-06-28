9536edae5b9959a1f75ecd85e9ad4a137dfbf35e
Mogeko <zhengjunyi@live.com>
2023-06-28 15:49:54 +0200

    fix: update changeset-version.js script

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -3,16 +3,15 @@
 const { execSync } = require("node:child_process");
 
 // This script is used by the `release.yml` workflow to update the version of the packages being released.
-// The standard step is only to run `changeset version` but this does not update the package-lock.json file.
+// The standard step is only to run `changeset version` but this does not update the pnpm-lock.yaml file.
 // So we also run `pnpm install`, which does this update.
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
 execSync("pnpm changeset version");
 
-// HACK: run a newer version of npm that can update package-lock.json using local dependencies.
-// See https://github.com/npm/cli/issues/4379 and https://github.com/npm/cli/pull/4371.
-// The error looks like: `No matching version found for wrangler@^2.15.0.`
-if (!process.version.startsWith("v16")) {
+// HACK: pnpm 8.x does not support node 14 yet.
+// See:https://pnpm.io/installation#compatibility
+if (process.version.startsWith("v14")) {
   throw new Error(
     "You have updated node version and so you must now remove the hack below!"
   );

cc25703df80946725ffbe74206748222fd0ef6e0
Mogeko <zhengjunyi@live.com>
2023-06-29 03:57:58 +0200

    feat: adjust the types

--- packages/tmdb-request/src/mod.ts
+++ packages/tmdb-request/src/mod.ts
@@ -1,4 +1,4 @@
-import { DEFAULTS, type RequestParameters } from "@/defaults";
+import { DEFAULTS, type RequestParams } from "@/defaults";
 import { parse, type Context } from "@/parse";
 
 async function fetcher({ url, ...options }: Endpoint) {
@@ -6,16 +6,16 @@ async function fetcher({ url, ...options }: Endpoint) {
 }
 
 export async function request<T>(route: string, opts: Context): Promise<T>;
-export async function request<T>(endpoint: EndpointParameters): Promise<T>;
+export async function request<T>(endpoint: EndpointParams): Promise<T>;
 export async function request(route: string, opts: Context): Promise<any>;
-export async function request(endpoint: EndpointParameters): Promise<any>;
-export async function request(route: string | EndpointParameters, opts = {}) {
+export async function request(endpoint: EndpointParams): Promise<any>;
+export async function request(route: string | EndpointParams, opts?: Context) {
   return fetcher(parser(route as any, opts));
 }
 
 export function parser(route: string, opts?: Context): Endpoint;
-export function parser(endpoint: EndpointParameters): Endpoint;
-export function parser(route: string | EndpointParameters, opts = {}) {
+export function parser(endpoint: EndpointParams): Endpoint;
+export function parser(route: string | EndpointParams, opts?: Context) {
   const parserInside = parse(DEFAULTS);
 
   if (typeof route === "string") {
@@ -26,5 +26,5 @@ export function parser(route: string | EndpointParameters, opts = {}) {
 }
 
 type RequiredFields<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
-export type EndpointParameters = RequiredFields<Context, "url">;
-export type Endpoint = RequiredFields<RequestParameters, "url">;
+export type EndpointParams = RequiredFields<Context, "url">;
+export type Endpoint = RequiredFields<RequestParams, "url">;

70d4402b3cbfffe37f6fc72794858f0270810c42
Mogeko <zhengjunyi@live.com>
2023-06-29 19:30:41 +0200

    feat: implement the wrapping of parser and request

--- packages/tmdb-api/src/mod.ts
+++ packages/tmdb-api/src/mod.ts
@@ -1,13 +1,28 @@
-export function tmdb() {
-  return "Hello World";
+import { parser, request, type Options } from "tmdb-request";
+import { mergeDeep } from "tmdb-request/merge-deep";
+
+export class TMDb {
+  private _defaultsOpts: Options;
+
+  constructor({ auth }: { auth: string }) {
+    this._defaultsOpts = { headers: { authorization: auth } };
+  }
+
+  public request(route: string, opts: Options = {}) {
+    return request(route, mergeDeep(this._defaultsOpts, opts));
+  }
+
+  public parse(route: string, opts: Options = {}) {
+    return parser(route, mergeDeep(this._defaultsOpts, opts));
+  }
 }
 
 if (import.meta.vitest) {
-  const { describe, it, expect } = await import("vitest");
+  const { describe, it } = await import("vitest");
 
-  describe("main", () => {
-    it("should return Hello World", () => {
-      expect(tmdb()).toBe("Hello World");
-    });
+  describe("TMDb", () => {
+    it.todo("should create a TMDb instance");
+    it.todo("should request");
+    it.todo("should parse");
   });
 }
